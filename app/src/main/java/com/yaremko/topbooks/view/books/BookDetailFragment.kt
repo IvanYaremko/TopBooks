56f033ea504ce6f95fb290a013d4b235c2d4bef7
Ivan Yaremko <ivan.yaremko@gmail.com>
2020-05-19 19:26:40 +0100

    added Palette functionality to change background colour based on the book selected

--- app/src/main/java/com/yaremko/topbooks/view/books/BookDetailFragment.kt
+++ app/src/main/java/com/yaremko/topbooks/view/books/BookDetailFragment.kt
@@ -1,11 +1,20 @@
 package com.yaremko.topbooks.view.books
 
+import android.graphics.drawable.Drawable
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.request.transition.Transition
+
+
+
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
 
 import com.yaremko.topbooks.R
 import com.yaremko.topbooks.model.Books
@@ -39,12 +48,32 @@ class BookDetailFragment : Fragment() {
                     context -> getProgressDrawable(context) }?.let {
                     progressDrawable -> detailImage.loadImage(it, progressDrawable)
             }
+            setBackgroundColour(it)
         }
 
         book.bookAuthor?.let {
             detailTitle.text = it
         }
 
+    }
+
+    private fun setBackgroundColour(url: String?) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource).generate() {
+                        palette ->
+                            val intColour = palette?.lightMutedSwatch?.rgb ?: 0
+                            detailLayout.setBackgroundColor(intColour)
+                    }
+                }
+            })
     }
 
 }
